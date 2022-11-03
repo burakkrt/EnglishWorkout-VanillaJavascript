2d6fa95c1a56014723e5fae3392e3ce88bc085d4
Burak <burak.75.kurt@gmail.com>
2022-11-03 23:40:25 +0300

    #sayilardaki width verme özelliği kaldırıldı. # sayila ve alfabe diğerleri ile aynı stile getirildi.

--- assets/js/sayilar-alfabe.js
+++ assets/js/sayilar-alfabe.js
@@ -1,25 +1,25 @@
 const sayilarAlfabeSorular = [
-    {id:0,  soru:"Zero, Two, Sixteen",           cevap:"0, 2, 16",   soruTurkce:"Zirou, Tu, Sikstiin",       cevapTurkce:"", type:"sayi"},
-    {id:1,  soru:"Eighteen, Nine, Fourteen",     cevap:"18, 9, 14",  soruTurkce:"Eytiin, Nayn, Fortiin",     cevapTurkce:"", type:"sayi"},
-    {id:2,  soru:"Three, Twelve, Six",           cevap:"3, 12, 6",   soruTurkce:"Tıri, Tvelf, Siks",         cevapTurkce:"", type:"sayi"},
-    {id:3,  soru:"Twenty, Three, Seventeen",     cevap:"20, 3, 17",  soruTurkce:"Tıventi, Tıri, Sevıntiin",  cevapTurkce:"", type:"sayi"},
-    {id:4,  soru:"Eleven, Fifteen, Ten",         cevap:"11, 15, 10", soruTurkce:"Elevın, Fiftiin, Ten",      cevapTurkce:"", type:"sayi"},
-    {id:5,  soru:"Two, Nineteen, Thirteen",      cevap:"2, 19, 13",  soruTurkce:"Tu, Nayntiin, Törtiin",     cevapTurkce:"", type:"sayi"},
-    {id:6,  soru:"Twelve, Twenty, Seventeen",    cevap:"12, 20, 17", soruTurkce:"Tvelf, Tıventi, Sevıntiin", cevapTurkce:"", type:"sayi"},
-    {id:7,  soru:"Four, Nineteen, Five",         cevap:"4, 19, 5",   soruTurkce:"For, Nayntiin, Fayf",       cevapTurkce:"", type:"sayi"},
-    {id:8,  soru:"Zero, Thirteen, Twelve",       cevap:"0, 13, 12",  soruTurkce:"Zirou, Törtiin, Tvelf",     cevapTurkce:"", type:"sayi"},
-    {id:9,  soru:"Four, Thirteen, Twelve",       cevap:"4, 13, 12",  soruTurkce:"For, Törtiin, Tvelf",       cevapTurkce:"", type:"sayi"},
-    {id:10, soru:"Twenty, Three, Fifteen",       cevap:"20, 3, 15",  soruTurkce:"Tıventi, Tıri, Fiftiin",    cevapTurkce:"", type:"sayi"},
-    {id:11, soru:"Thirteen, Eight, Nineteen",    cevap:"13, 8, 19",  soruTurkce:"Törtiin, Eyt, Nayntiin",    cevapTurkce:"", type:"sayi"},
-    {id:12, soru:"Eighteen, Sixteen, Eleven",    cevap:"18, 16, 11", soruTurkce:"Eytiin, Sikstiin, Elevın",  cevapTurkce:"", type:"sayi"},
-    {id:13, soru:"Seventeen, Two, Twenty",       cevap:"17, 2, 20",  soruTurkce:"Sevıntiin, Tu, Tıventi",    cevapTurkce:"", type:"sayi"},
-    {id:14, soru:"Thirteen, Three, Twelve",      cevap:"13, 3, 12",  soruTurkce:"Törtiin, Tıri, Tvelf",      cevapTurkce:"", type:"sayi"},
-    {id:15, soru:"Fiat",        cevap:"ef, ay, ey, ti",                         soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:"", type:"alfabe"},
-    {id:16, soru:"Mitsubisi",   cevap:"em, ay, ti, es, yu, bi, ay, es, ay",     soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:"", type:"alfabe"},
-    {id:17, soru:"Hygrapy",     cevap:"eyc, vay, ci, ar, ey, pi, vay",          soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:"", type:"alfabe"},
-    {id:18, soru:"Overview",    cevap:"ou, vi, i, ar, vi, ay, i, dablyu",       soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:"", type:"alfabe"},
-    {id:19, soru:"Director",    cevap:"di, ay, ar, i, si, ti, ou, ar",          soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:"", type:"alfabe"},
-    {id:20, soru:"Chief",       cevap:"si, eyc, ay, i, ef",                     soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:"", type:"alfabe"},
+    {id:0,  soru:"Zero, Two, Sixteen",           cevap:"0, 2, 16",   soruTurkce:"Zirou, Tu, Sikstiin",       cevapTurkce:""},
+    {id:1,  soru:"Eighteen, Nine, Fourteen",     cevap:"18, 9, 14",  soruTurkce:"Eytiin, Nayn, Fortiin",     cevapTurkce:""},
+    {id:2,  soru:"Three, Twelve, Six",           cevap:"3, 12, 6",   soruTurkce:"Tıri, Tvelf, Siks",         cevapTurkce:""},
+    {id:3,  soru:"Twenty, Three, Seventeen",     cevap:"20, 3, 17",  soruTurkce:"Tıventi, Tıri, Sevıntiin",  cevapTurkce:""},
+    {id:4,  soru:"Eleven, Fifteen, Ten",         cevap:"11, 15, 10", soruTurkce:"Elevın, Fiftiin, Ten",      cevapTurkce:""},
+    {id:5,  soru:"Two, Nineteen, Thirteen",      cevap:"2, 19, 13",  soruTurkce:"Tu, Nayntiin, Törtiin",     cevapTurkce:""},
+    {id:6,  soru:"Twelve, Twenty, Seventeen",    cevap:"12, 20, 17", soruTurkce:"Tvelf, Tıventi, Sevıntiin", cevapTurkce:""},
+    {id:7,  soru:"Four, Nineteen, Five",         cevap:"4, 19, 5",   soruTurkce:"For, Nayntiin, Fayf",       cevapTurkce:""},
+    {id:8,  soru:"Zero, Thirteen, Twelve",       cevap:"0, 13, 12",  soruTurkce:"Zirou, Törtiin, Tvelf",     cevapTurkce:""},
+    {id:9,  soru:"Four, Thirteen, Twelve",       cevap:"4, 13, 12",  soruTurkce:"For, Törtiin, Tvelf",       cevapTurkce:""},
+    {id:10, soru:"Twenty, Three, Fifteen",       cevap:"20, 3, 15",  soruTurkce:"Tıventi, Tıri, Fiftiin",    cevapTurkce:""},
+    {id:11, soru:"Thirteen, Eight, Nineteen",    cevap:"13, 8, 19",  soruTurkce:"Törtiin, Eyt, Nayntiin",    cevapTurkce:""},
+    {id:12, soru:"Eighteen, Sixteen, Eleven",    cevap:"18, 16, 11", soruTurkce:"Eytiin, Sikstiin, Elevın",  cevapTurkce:""},
+    {id:13, soru:"Seventeen, Two, Twenty",       cevap:"17, 2, 20",  soruTurkce:"Sevıntiin, Tu, Tıventi",    cevapTurkce:""},
+    {id:14, soru:"Thirteen, Three, Twelve",      cevap:"13, 3, 12",  soruTurkce:"Törtiin, Tıri, Tvelf",      cevapTurkce:""},
+    {id:15, soru:"Fiat",        cevap:"ef, ay, ey, ti",                         soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:""},
+    {id:16, soru:"Mitsubisi",   cevap:"em, ay, ti, es, yu, bi, ay, es, ay",     soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:""},
+    {id:17, soru:"Hygrapy",     cevap:"eyc, vay, ci, ar, ey, pi, vay",          soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:""},
+    {id:18, soru:"Overview",    cevap:"ou, vi, i, ar, vi, ay, i, dablyu",       soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:""},
+    {id:19, soru:"Director",    cevap:"di, ay, ar, i, si, ti, ou, ar",          soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:""},
+    {id:20, soru:"Chief",       cevap:"si, eyc, ay, i, ef",                     soruTurkce:"Yukarıdaki kelimenin harflerini ingilizce okunuşu ile yazınız.", cevapTurkce:""},
 
 ];
 
@@ -90,152 +90,59 @@ document.getElementById(soruOlusturButtonID3).addEventListener("click", () =>{
     // --Sadece soru checkbox checked.Eğer sadece soru sorulup cevap istenecek ise.
     if(soruTipi == false) randomQuestionOrAnswer = 0;
 
-    // ------ Soru Tipine Göre Soru Oluşturma ------
-    if(question.type == "sayi"){
-
-        // -- Soruları ve Türkçe söyleyişlerini harf uzunluğunun katları olarak hesaplayıp width değerine yazdırma. --
-        let widthPixel = [];
-
-        const splitSoruArray = question.soru.replace(" ","").split(",");
-        let splitSoruContent = ``;
-        
-        const turkishSplitSoruArray = question.soruTurkce.replace(" ","").split(",");
-        let turkishSplitSoruContent = ``;
-
-        splitSoruArray.map ( (soru) => { 
-            // Eğer Mobil 'de değilse genişlik çarpanlarını daha fazla ver.
-            if(window.matchMedia("(min-width: 1360px)").matches){
-
-                if(soru.length <= 13 && soru.length > 7) widthPixel.push((soru.length) * 13);
-                else if(soru.length <= 7 && soru.length > 5) widthPixel.push((soru.length) * 27);
-                else if(soru.length < 5) widthPixel.push((soru.length) * 40);
-                else widthPixel.push((soru.length) * 12);
-            }
-            else {
-
-                if(soru.length < 10) widthPixel.push((soru.length) * 11);
-                else widthPixel.push((soru.length) * 9); 
-            }
-        });
-
-
-        for(let i = 0 ; i < splitSoruArray.length ; i ++){
-            
-            splitSoruContent += `<span style="width: ${widthPixel[i]}px;">${splitSoruArray[i]}</span>`;
-            turkishSplitSoruContent += `<span style="width: ${widthPixel[i]}px;">${turkishSplitSoruArray[i]}</span>`;
-        }
-
-        // ------------- Cevap Bilinmiyor İse ---------------
-        if(randomQuestionOrAnswer == 0){
-            document.getElementById("allContainer").insertAdjacentHTML("beforeend", 
-            `
-            <section class="soru-item" id="${randomSoruID}">
-                <div class="settings">
-                    <span class="material-symbols-outlined settings-icon trueOrFalseCheck" onclick="${answerCheckFunctionName3}(${question.id},${randomSoruID},true)">check</span>
+     // ------------- Cevap Bilinmiyor İse ---------------
+     if(randomQuestionOrAnswer == 0){
+        document.getElementById("allContainer").insertAdjacentHTML("beforeend", 
+        `
+        <section class="soru-item" id="${randomSoruID}">
+            <div class="settings">
+                <span class="material-symbols-outlined settings-icon trueOrFalseCheck" onclick="${answerCheckFunctionName3}(${question.id},${randomSoruID},true)">check</span>
+            </div>
+            <div class="soru">
+                <span class="material-symbols-outlined soru-icon">quiz</span>
+                <div class="soru-content" id="soru-content">
+                ${question.soru}
+                <span class="turkish">${question.soruTurkce}</span>
                 </div>
-                <div class="soru">
-                    <span class="material-symbols-outlined soru-icon">quiz</span>
-                    <div class="soru-content" id="soru-content">
-                    <span class="space-numbers">${splitSoruContent}</span>
-                    <span class="turkish space-numbers-turkish">${turkishSplitSoruContent}</span>
-                    </div>
-                </div>
-                <div class="cevap">
-                    <span class="material-symbols-outlined soru-icon">record_voice_over</span>
-                    <div class="cevap-content" id="cevap-content">
-                        <input type="text" onkeypress="keyPressEnter(event)" spellcheck="false"></input>
-                        <span class="turkish">${question.cevapTurkce}</span>
-                    </div>
-                </div>
-            </section>
-            `
-            );
-        }
-
-        // ------------- Soru Bilinmiyor İse ---------------
-        if(randomQuestionOrAnswer == 1){
-
-            document.getElementById("allContainer").insertAdjacentHTML("beforeend", 
-            `
-            <section class="soru-item" id="${randomSoruID}">
-                <div class="settings">
-                    <span class="material-symbols-outlined settings-icon trueOrFalseCheck" onclick="${answerCheckFunctionName3}(${question.id},${randomSoruID},false)">check</span>
-                </div>
-                <div class="soru">
-                    <span class="material-symbols-outlined soru-icon">quiz</span>
-                    <div class="soru-content" id="soru-content">
+            </div>
+            <div class="cevap">
+                <span class="material-symbols-outlined soru-icon">record_voice_over</span>
+                <div class="cevap-content" id="cevap-content">
                     <input type="text" onkeypress="keyPressEnter(event)" spellcheck="false"></input>
-                    <span class="turkish"></span>
-                    </div>
-                </div>
-                <div class="cevap">
-                    <span class="material-symbols-outlined soru-icon">record_voice_over</span>
-                    <div class="cevap-content" id="cevap-content">
-                    ${question.cevap}
                     <span class="turkish">${question.cevapTurkce}</span>
-                    </div>
                 </div>
-            </section>
-            `
-            );
-        }
+            </div>
+        </section>
+        `
+        );
     }
-    else if (question.type == "alfabe"){
 
-        // ------------- Cevap Bilinmiyor İse ---------------
-        if(randomQuestionOrAnswer == 0){
-            document.getElementById("allContainer").insertAdjacentHTML("beforeend", 
-            `
-            <section class="soru-item" id="${randomSoruID}">
-                <div class="settings">
-                    <span class="material-symbols-outlined settings-icon trueOrFalseCheck" onclick="${answerCheckFunctionName3}(${question.id},${randomSoruID},true)">check</span>
-                </div>
-                <div class="soru">
-                    <span class="material-symbols-outlined soru-icon">quiz</span>
-                    <div class="soru-content" id="soru-content">
-                    ${question.soru}
-                    <span class="turkish">${question.soruTurkce}</span>
-                    </div>
-                </div>
-                <div class="cevap">
-                    <span class="material-symbols-outlined soru-icon">record_voice_over</span>
-                    <div class="cevap-content" id="cevap-content">
-                        <input type="text" onkeypress="keyPressEnter(event)" spellcheck="false"></input>
-                        <span class="turkish">${question.cevapTurkce}</span>
-                    </div>
-                </div>
-            </section>
-            `
-            );
-        }
+    // ------------- Soru Bilinmiyor İse ---------------
+    if(randomQuestionOrAnswer == 1){
 
-        // ------------- Soru Bilinmiyor İse ---------------
-        if(randomQuestionOrAnswer == 1){
-
-            document.getElementById("allContainer").insertAdjacentHTML("beforeend", 
-            `
-            <section class="soru-item" id="${randomSoruID}">
-                <div class="settings">
-                    <span class="material-symbols-outlined settings-icon trueOrFalseCheck" onclick="${answerCheckFunctionName3}(${question.id},${randomSoruID},false)">check</span>
+        document.getElementById("allContainer").insertAdjacentHTML("beforeend", 
+        `
+        <section class="soru-item" id="${randomSoruID}">
+            <div class="settings">
+                <span class="material-symbols-outlined settings-icon trueOrFalseCheck" onclick="${answerCheckFunctionName3}(${question.id},${randomSoruID},false)">check</span>
+            </div>
+            <div class="soru">
+                <span class="material-symbols-outlined soru-icon">quiz</span>
+                <div class="soru-content" id="soru-content">
+                <input type="text" onkeypress="keyPressEnter(event)" spellcheck="false"></input>
+                <span class="turkish"></span>
                 </div>
-                <div class="soru">
-                    <span class="material-symbols-outlined soru-icon">quiz</span>
-                    <div class="soru-content" id="soru-content">
-                    <input type="text" onkeypress="keyPressEnter(event)" spellcheck="false"></input>
-                    <span class="turkish"></span>
-                    </div>
+            </div>
+            <div class="cevap">
+                <span class="material-symbols-outlined soru-icon">record_voice_over</span>
+                <div class="cevap-content" id="cevap-content">
+                ${question.cevap}
+                <span class="turkish">${question.cevapTurkce}</span>
                 </div>
-                <div class="cevap">
-                    <span class="material-symbols-outlined soru-icon">record_voice_over</span>
-                    <div class="cevap-content" id="cevap-content">
-                    ${question.cevap}
-                    <span class="turkish">${question.cevapTurkce}</span>
-                    </div>
-                </div>
-            </section>
-            `
-            );
-        }
+            </div>
+        </section>
+        `
+        );
     }
 
     // Soru oluşturulduktan sonra sonuncu sorunun içerisindeki inputa odaklan.
